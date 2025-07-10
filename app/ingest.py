5659a99d26bf281f953b3b673b8b239662e4c474
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-10 20:17:02 +0100

    ingest and main scripts

--- app/ingest.py
+++ app/ingest.py
@@ -1,2 +1,29 @@
+# this script is like one time process to prepare the documents for interpretation (framework for live app)
+# 1. loads docs from /data
+# 2. splits them into parts
+# 3. converts into embeddings using OpenAI
+
 from langchain.document_loaders import TextLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitterTextSplitter
+from langchain.vectorstores import FAISS
+
+load_dotenv()
+
+filename = input('Enter the path to PDF: ') #loading the doc
+loader = PyPDFLoader(filename) #reading the doc
+
+splitter = RecursiveCharacterTextSplitterTextSplitter( #recursive for smart text splitting 
+    chunk_size=500,
+    chunk_overlap=100
+) #500 and 100 are a good starting point for PDFs
+
+docs = splitter.split_documents(loader) #splits the doc chosen
+embeddings = OpenAIEmbeddings() #chunks into embeddings (OpenAI key)
+
+vectors = FAISS.from_documents(docs, embeddings) #storing in vector database
+vectors.save_local('faiss_index') #storing locally
+
 
