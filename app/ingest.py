6f5574e856197e74d64a4239c7f36305c0a53bf4
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-14 10:37:55 +0100

    fixed ingest.py, added faiss.index

--- app/ingest.py
+++ app/ingest.py
@@ -3,27 +3,31 @@
 # 2. splits them into parts
 # 3. converts into embeddings using OpenAI
 
-from langchain.document_loaders import TextLoader
-from langchain.document_loaders import PyPDFLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
-from langchain.text_splitter import RecursiveCharacterTextSplitterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+import os
 
 load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY') # loads .env file 
 
 filename = input('Enter the path to PDF: ') #loading the doc
 loader = PyPDFLoader(filename) #reading the doc
+documents = loader.load()
 
-splitter = RecursiveCharacterTextSplitterTextSplitter( #recursive for smart text splitting 
+splitter = RecursiveCharacterTextSplitter( #recursive for smart text splitting 
     chunk_size=500,
     chunk_overlap=100
 ) #500 and 100 are a good starting point for PDFs
 
-docs = splitter.split_documents(loader) #splits the doc chosen
-embeddings = OpenAIEmbeddings() #chunks into embeddings (OpenAI key)
+docs = splitter.split_documents(documents) #splits the doc chosen
+embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key) #chunks into embeddings (OpenAI key)
 
 vectors = FAISS.from_documents(docs, embeddings) #storing in vector database
 vectors.save_local('faiss_index') #storing locally
+print("FAISS index saved locally as 'faiss_index'")
+
 
 
