2cc6910f65cde2f0452c4479d68fffdf690cbddc
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-17 12:36:43 +0100

    final "module not found" fix

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from main import core_components
+from .main import core_components
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
