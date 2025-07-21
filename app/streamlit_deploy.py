35068cc3241cf3d6020164d16750eed55c8630ef
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-21 12:27:27 +0100

    fixes before deployment

--- app/streamlit_deploy.py
+++ app/streamlit_deploy.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from api import ask_q
 from main import core_components
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
@@ -6,7 +7,7 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.base import BaseCallbackHandler
 
 if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = [] # if there is no history, create empty listt
+    st.session_state.chat_history = [] # if ther is no history, create empty listt
 
 llm, retriever, reasoning_template, embeddings, db = core_components() #unpacking components (for streaming)
 
@@ -28,7 +29,7 @@ for m in st.session_state.chat_history:
     with st.chat_message(m['role']): # role => user or AI
         st.markdown(m['content']) # st.markdown to print the text out
 
-#to figure out user's name for later use in the chat 
+#to figure out user's name for later use in the chat
 if 'name' not in st.session_state:
     st.session_state.name = st.text_input("What's your name?")
     st.stop()
@@ -38,27 +39,20 @@ upload = st.file_uploader("Choose the doc") # for uploading own docs
 inp = st.chat_input(f'What do you want to know, {st.session_state.name}?')
 if inp: #trigerring chat response
     st.chat_message('user').markdown(inp) # show user input
-
+    st.session_state.messages.append({'role': 'user', 'content': inp}) # adding new message to the list that stores converstion
     with st.chat_message('AI'):
         r_container = st.empty() # this creates container for streaming; it is space that streaming will gradually be filling up: for showing on page
 
         partial = '' #each answer will begin from empty string and then grow in it, every time from strach; for scratching before showing
-        
+
         class StreamHandler(BaseCallbackHandler):
-            def __init__(self, r_container):
-                self.r_container = r_container
-                self.partial = ""
-
-            def on_llm_new_token(self, token: str, *, chunk = None, parent_run_id = None, **kwargs):
-                self.partial += token
-                self.r_container.markdown(self.partial)
+            def on_llm_new_token(self, token=str, *, chunk = None, parent_run_id = None, **kwargs):
+                
+                partial += token
+                r_container.markdown(partial)
                 return super().on_llm_new_token(token, chunk=chunk, parent_run_id=parent_run_id, **kwargs)
-
-        #creating actual streaming        
+                
         with st.spinner('Reasoning...'): # loading animation
-            handler = StreamHandler(r_container)
-            qa_chain.callbacks = [handler]
-            answer = qa_chain.run(inp)
-    st.session_state.chat_history.append({'role': 'user', 'content': inp}) # saving USER's input to chat history
-    st.session_state.chat_history.append({'role': 'assistant', 'content': answer}) # saving ASSISTANT's response to chat history, it definitely will
- 
+            answer = ask_q(inp) # calling ASK_Q function from API module
+            st.markdown(answer) # returning result
+    st.session_state.messages.append({'role': 'assistant', 'content': answer}) # saving ASSISTANT's response to chat history
