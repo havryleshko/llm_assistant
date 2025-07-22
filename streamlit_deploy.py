419fa425b62d8a9647c9d6e99883aa24d9770c67
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-22 11:56:39 +0100

    api func fix

--- streamlit_deploy.py
+++ streamlit_deploy.py
@@ -61,6 +61,6 @@ if inp: #trigerring chat response
                 
         with st.spinner('Reasoning...'): # loading animation
             chat_id = st.session_state.name
-            answer = asyncio.run(ask_q(Question(question=inp, chat_id=chat_id), callbacks=[StreamHandler(r_container)])) # calling ASK_Q function from API module
+            answer = ask_q(Question(question=inp, chat_id=chat_id), callbacks=[StreamHandler(r_container)]) # calling ASK_Q function from API module
             r_container.markdown(answer['answer']) # returning result
     st.session_state.chat_history.append({'role': 'assistant', 'content': answer}) # saving ASSISTANT's response to chat history
