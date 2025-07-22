5313cd03686ca98a21c2272715a00468cfca81f6
Alex Havryleshko <alexhavryleshko@gmail.com>
2025-07-22 12:47:41 +0100

    fix in api

--- api.py
+++ api.py
@@ -37,5 +37,5 @@ def ask_q(q: Question, callbacks=None):
         combine_docs_chain_kwargs={'prompt': reasoning_template},
         callbacks=callbacks
     )
-    answer = qa_chain.run(q.question)
+    answer = qa_chain.run({'question': q.question})
     return answer
